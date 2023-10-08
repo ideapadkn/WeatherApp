c14707a6ba3f03e4fd8c8da0339835237cad3f55
Kamron or Камрон <kamrush6429@gmail.com>
2023-10-08 18:31:19 +0500

    fix

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+#app {
+  background-image: url('https://avatars.mds.yandex.net/i?id=5c5de7eddfa0feee04fc69bc1ce55d7395f8b484-10385932-images-thumbs&n=13');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+
+const apiKey = ref('2ad213d1b639694377f42b93faafc162')
+
+
+</script>
+
+<template>
+  <main class="main">
+    <div class="container mx-auto pt-5">
+      <div class="search-box">
+        <input type="text" class="search-bar block w-full p-4 border-none outline-none" placeholder="search...">
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+.main {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+  height: 100vh;
+}
+
+.search-bar {
+  color: #313131;
+  font-size: 20px;
+  border-radius: 0 16px 0 16px;
+  background-color: rgba(255, 255, 255, .5);
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, .25);
+  transition: .4s;
+
+  &:focus {
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, .25);
+    background-color: rgba(255, 255, 255, .9);
+    border-radius: 16px 0 16px 0;
+
+  }
+}
+</style>
