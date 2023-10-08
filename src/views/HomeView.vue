73e620f99c4200d49503f800132353907f5732cb
Kamron or Камрон <kamrush6429@gmail.com>
2023-10-08 19:03:21 +0500

    interface finish

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,41 @@
 import { ref } from 'vue';
 
 const apiKey = ref('2ad213d1b639694377f42b93faafc162')
+const urlBase = ref('https://api.openweathermap.org/data/3.0/')
+const query = ref('')
+const weather = ref({})
 
+const fetchWeather = (e) => {
+  if (e.key == 'Enter') {
+    fetch(`${urlBase.value}onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey.value}`)
+      .then(res => {
+        return res.json()
+      }).then(setResults)
+  }
+}
+
+const setResults = (results) => {
+  weather.value = results
+}
 
 </script>
 
 <template>
   <main class="main">
     <div class="container mx-auto pt-5">
-      <div class="search-box">
-        <input type="text" class="search-bar block w-full p-4 border-none outline-none" placeholder="search...">
+      <div class="search-box mb-5">
+        <input class="search-bar block w-full p-4 border-none outline-none" v-model="query" @keypress="fetchWeather"
+          type="text" placeholder="search...">
+      </div>
+      <div class="weather-wrap" v-if="(typeof weather.main != 'undefined')">
+        <div class="location-box text-center mb-5">
+          <div class="location text-white text-3xl font-medium mb-1">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date text-white text-lg font-light italic">Monday 20 January 2023</div>
+        </div>
+        <div class="weather-box text-center">
+          <div class="temp py-3 px-6 text-white text-8xl font-black rounded-2xl inline-block mb-5">9^</div>
+          <div class="weather text-white text-5xl font-bold italic">Rain</div>
+        </div>
       </div>
     </div>
   </main>
@@ -36,5 +62,20 @@ const apiKey = ref('2ad213d1b639694377f42b93faafc162')
     border-radius: 16px 0 16px 0;
 
   }
+}
+
+.location {
+  text-shadow: 1px 3px rgba(0, 0, 0, .25);
+}
+
+.temp {
+  text-shadow: 3px 6px rgba(0, 0, 0, .25);
+  background-color: rgba(255, 255, 255, .25);
+  box-shadow: 3px 6px rgba(0, 0, 0, .25);
+}
+
+.weather {
+  text-shadow: 3px 6px rgba(0, 0, 0, .25);
+
 }
 </style>
